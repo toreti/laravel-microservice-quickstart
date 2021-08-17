dfec648b2c7ac53dc9c6b4a06bcd4e7ce5f69385
Felipe Torretto <toreti@gmail.com>
2021-08-16 21:28:34 -0300

    feat: categories resource

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Models\Category::class, 10)->create();
+    }
+}
